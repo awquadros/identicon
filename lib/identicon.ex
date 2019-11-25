6dcdf4ad9ca27d1ed766e2f36ca0e494880650de
Augusto Quadros <augusto.wfq@gmail.com>
2019-11-24 23:22:12 -0300

    image generation finished

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -21,6 +21,10 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
   end
 
   def hash_input(input) do
@@ -42,7 +46,7 @@ defmodule Identicon do
   end
 
   def mirror_row([first, second | _tail] = row) do
-    row ++ [first, second]
+    row ++ [second, first]
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
@@ -51,14 +55,14 @@ defmodule Identicon do
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
     grid = Enum.filter grid, fn({code, _image}) ->
-      rem(code, 2)
+      rem(code, 2) == 0
     end
 
     %Identicon.Image{image | grid: grid}
   end
 
-  def build_pixel_map(%Identicon.Image{grid: grid} = _image) do
-    Enum.map grid, fn({_code, index}) ->
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
       horizontal = rem(index, 5) * 50
       vertical = div(index, 5) * 50
 
@@ -67,5 +71,22 @@ defmodule Identicon do
 
       {top_left, bottom_right}
     end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250,250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
   end
 end
--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -5,4 +5,13 @@ defmodule IdenticonTest do
   test "greets the world" do
     assert Identicon.hello() == :world
   end
+
+  test "pick color from the first three elements of a given list" do
+    image_with_color = Identicon.pick_color(%Identicon.Image{hex: [12,45,78,98,65,32]})
+    assert image_with_color.color == {12,45,78}
+  end
+
+  test "mirror a row of three elements" do
+    assert Identicon.mirror_row([123,456,789]) == [123,456,789,456,123]
+  end
 end
